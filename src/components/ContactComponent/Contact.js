3296dc0c410638a95552b730e4ee7ff08fe3909c
mushfiqur06 <mushfiqur06@gmail.com>
2018-10-01 09:26:54 +0600

    A single Contact skeleton

--- src/components/ContactComponent/Contact.js
+++ src/components/ContactComponent/Contact.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
-import css from './Contact.css'
+import './Contact.css'
 
 class Contact extends Component{
+    
     render(){
+        console.log(this.props.deletehandler(this.props.contact))
         return (
             <div className='col-sm-4'>
                 <div className='singleContactWrapper'>
                     <div className='contactPersonImg text-center'>
-                        <i class="fas fa-user-tie"></i>
+                        <i className="fas fa-user-tie"></i>
                     </div>
                     <div className='contactPersonInfo text-center'>
                         <h2>{this.props.contact.name}</h2>
-                        <p><i class="fas fa-map-marker-alt"></i> Kisko, Finland</p>
-                        <h3><i class="far fa-envelope"></i> <a>{this.props.contact.email}</a></h3>
-                        <h3 className='phone'><i class="fas fa-phone-volume"></i> {this.props.contact.phone}</h3>
+                        <p><i className="fas fa-map-marker-alt"></i> Kisko, Finland</p>
+                        <h3><i className="far fa-envelope"></i> <a>{this.props.contact.email}</a></h3>
+                        <h3 className='phone'><i className="fas fa-phone-volume"></i> {this.props.contact.phone}</h3>
                     </div>
                     <div className='contactUtility text-center'>
-                        <div className='moreInfo'>
+                        <div className='moreInfo' onClick={this.props.editehandleOpen}>
                             <p>More Info</p>
-                            <i class="fas fa-long-arrow-alt-right"></i>
+                            <i className="fas fa-long-arrow-alt-right"></i>
                         </div>
-                        <div className='editInfo'>
-                            <i class="far fa-edit" title='For Edit'></i>
+                        <div className='editInfo' >
+                            <i className="far fa-edit" title='For Edit'></i>
                         </div>
-                        <div className='deleteInfo'>
-                            <i class="far fa-trash-alt" title='For Delete'></i>
+
+                        <div className='deleteInfo' onClick={() => this.props.deletehandler(this.props.contact)}>
+                            <i className="far fa-trash-alt" title='For Delete'></i>
                         </div>
-                        <div className='likeInfo'>
-                            {this.props.contact.isFavorite ? <i class="fas fa-thumbs-up" title='For Unfavorite'></i> : <i class="far fa-thumbs-up" title='For Fovrite'></i>}
+
+                        <div className='likeInfo' onClick={() => this.props.favouriteHandler(this.props.contact)}>
+                            {this.props.contact.isFavorite ? <i className="fas fa-thumbs-up" title='For Unfavorite'></i> : <i className="far fa-thumbs-up" title='For Fovrite'></i>}
                         </div>
                     </div>
                 </div>
